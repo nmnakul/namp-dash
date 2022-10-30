f1b5bca17baceb060afca1eb6eaeeb3007b99d16
nmnakul <nmnakul@gmail.com>
2022-10-30 12:59:29 +0530

    created "year & accuracy" and setup combobox

---
+++ src/lib/stateful/Graph-Year-And-Accuracy.svelte
@@ -0,0 +1,26 @@
+<section class="pb-10">
+    <ComboBox
+    titleText="City"
+    placeholder="Select a city to view city level analysis"
+    bind:selectedId
+    items={cities}
+    />
+</section>
+
+<script>
+    import { Chart, LineController, registerables } from 'chart.js';
+    Chart.register(...registerables);
+    import { onMount } from 'svelte';
+    import { ComboBox, Button } from "carbon-components-svelte";
+
+    let selectedId = "1";
+    let cities = [];
+    
+    onMount(async () => {
+      const response = await fetch('./assets/cities-dataset.json');
+      let data = await response.json();
+      cities = data;
+    });
+
+</script>
+
